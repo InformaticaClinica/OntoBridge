75b8468300dfc047556e66a2fc81437e7fb0d192
GB <103437223+guibra95@users.noreply.github.com>
2026-06-20 20:20:49 +0200

    Refactor load_owl_fuseki to handle TTL files

--- services/ontobridge/utils/Fuseki_utils.py
+++ services/ontobridge/utils/Fuseki_utils.py
@@ -4,12 +4,28 @@ import requests
 def load_owl_fuseki(fuseki_url, ontologies_folder, new_dataset_name):
     dataset_url = f'{fuseki_url}{new_dataset_name}/data?default'
 
-    owl_files = [f for f in os.listdir(ontologies_folder) if f.endswith('.owl')]
+    # Filter OWL and TTL files
+    ontology_files = [os.path.join(ontologies_folder, f) 
+                      for f in os.listdir(ontologies_folder) if f.endswith(('.owl', '.ttl'))]
 
-    for file in owl_files:
-        owl_file_path = os.path.join(ontologies_folder, file)
-        with open(owl_file_path, 'r', encoding='utf-8') as f:
-            owl_data = f.read()
+    for file_path in ontology_files:
+        # Determine content type based on file extension
+        if file_path.endswith('.owl'):
+            content_type = 'application/rdf+xml; charset=utf-8'
+        elif file_path.endswith('.ttl'):
+            content_type = 'text/turtle; charset=utf-8'
+        else:
+            continue  # This should not happen, but kept for safety
+
+        # Read the file and send it to Fuseki
+        with open(file_path, 'r', encoding='utf-8') as f:
+            ontology_data = f.read()
             dataset_url = f'{fuseki_url}{new_dataset_name}'
-            response = requests.post(dataset_url, data=owl_data.encode('utf-8'),
-                headers={'Content-Type': 'application/rdf+xml; charset=utf-8'})
+            response = requests.post(dataset_url, data=ontology_data.encode('utf-8'),
+                                     headers={'Content-Type': content_type})
+
+            # Check server response
+            if response.status_code == 200:
+                print(f"File {file_path} loaded successfully into {dataset_url}")
+            else:
+                print(f"Error loading {file_path}: {response.status_code} - {response.text}")
